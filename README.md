97a57761c7e49cd1b829070acb7df3304618a546
Sasha Kotlyar <arktronic@users.noreply.github.com>
2025-01-25 21:17:26 -0500

    Add build script and package.json

---
+++ README.md
@@ -0,0 +1,5 @@
+# sevenzip-wasm
+
+Just another npm package with 7-Zip compiled for WebAssembly, inspired by similar packages out there.
+
+Full description coming later.
---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+which emmake &>/dev/null || {
+	echo "FATAL ERROR: emmake not found. Emscripten has not been initialized."
+	exit 1
+}
+
+pushd 7zip &>/dev/null
+if [ ! -f "README.md" ]; then
+	echo "FATAL ERROR: 7zip submodule has not been initialized."
+	exit 1
+fi
+
+if git apply --reverse --check ../7zip-24.09.patch &>/dev/null; then
+	echo "Patch already applied. Proceeding."
+else
+	echo "Applying patch."
+	git apply ../7zip-24.09.patch
+fi
+popd &>/dev/null
+
+echo "Building."
+pushd 7zip/CPP/7zip/Bundles/Alone2 &>/dev/null
+emmake make -f ../../cmpl_gcc.mak
+popd &>/dev/null
+
+echo "Copying output to dist directory."
+cp -f 7zip/CPP/7zip/Bundles/Alone2/b/g/sevenzip-wasm* dist/
+
+echo "Done!"
